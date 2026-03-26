f612c3d9379f11710faa7856593f91460544e263
Sina Ghadri <info@sinaghadri.com>
2026-03-26 15:54:08 +0400

    fix(build): force dynamic rendering for app layout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { Sidebar } from "@/components/layout/sidebar";
 import { Header } from "@/components/layout/header";
 import { BottomNav } from "@/components/layout/bottom-nav";
